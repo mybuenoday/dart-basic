d568d4c18590b1b6ed450175f90bf638d665713b
mybuenoday <yoonjibiz@gmail.com>
2023-12-14 07:36:46 +0900

    현재 디렉토리는 더 상위 폴더이므로, sample 경로를 지정해줌. Future-async-await. readAsString & writeAsString, try-catch

--- lib/23_12_13/async.dart
+++ lib/23_12_13/async.dart
@@ -2,13 +2,18 @@ import 'dart:io';
 
 Future<void> modifyCopy(String fromPath, String toPath) async {
   try {
-    final sample = File('sample.csv');
+    /* final sample = File('sample.csv');
+    오류 PathNotFoundException: Cannot open file, path = 'sample.csv' (OS Error: The system cannot find the file specified.
+, errno = 2) 발생 */
+    print('현재 디렉토리: ${Directory.current.path}');
+    final sample = File('lib/23_12_13/$fromPath'); // 상대 경로
     final text = await sample.readAsString();
     print('sample text: $text');
 
     final modifiedText = text.replaceAll('한석봉', '김석봉');
 
-    final copiedFile = File(toPath);
+    // final copiedFile = File(toPath);
+    final copiedFile = File('lib/23_12_13/$toPath');
     await copiedFile.writeAsString(modifiedText);
 
     print('수정완료');
